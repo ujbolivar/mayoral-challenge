4e376e8130694b8e15b84e638ca88988e3e0af2a
Unai Bolivar <ujbolivar@gmail.com>
2023-01-01 20:28:27 +0100

    refactor: clean up files a bit

--- src/components/Spinner/Spinner.tsx
+++ src/components/Spinner/Spinner.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './Spinner.module.css';
 
-// create a spinner animation
 const Spinner = () => {
     return (
         <div className={styles.spinner}></div>
