1c0cb1b2f130e789d67d47d2122d8f5ad55fe4fa
Unai Bolivar <ujbolivar@gmail.com>
2023-01-03 18:06:29 +0100

    bug - Firefox render: Added a script in the _document.tsx Head that imports everything needed to let firefox render media containers

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,11 +1,13 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 const Document = () => {
     return (
         <Html lang="es">
             <Head>
                 <meta name="viewport" content="initial-scale=1, width=device-width" />
-                <link
+                <Script src="https://cdn.jsdelivr.net/npm/container-query-polyfill@1/dist/container-query-polyfill.modern.js"></Script>
+				<link
                     rel="stylesheet"
                     href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
                 />
