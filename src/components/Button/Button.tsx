69481a4e5f26e2f3a0802b8053a629c91e4ab475
Unai Bolivar <ujbolivar@gmail.com>
2023-01-06 15:55:32 +0100

    refactor - Button component: recfactor the conditional style rendering inside the className attribute and discard needless repetitive code.

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,24 +2,9 @@ import styles from './Button.module.css';
 
 const Button = ({ name, category, variant, text, action }) => {
     return (
-        <>
-            {category === 'electronics' && variant === 'buttonAlt' ? (
-                <button className={styles.ButtonNone} value={text} onClick={action}>
+                <button name={name} className={`${variant === 'button' ? styles.Button : variant === 'buttonAlt' && category === 'electronics' ? styles.ButtonNone : styles.ButtonAlt}`} value={text} onClick={action}>
                     {text}
                 </button>
-            ) : variant === 'button' ? (
-                <button name={name} className={styles.Button} value={text} onClick={action}>
-                    {text}
-                </button>
-            ) : variant === 'buttonAlt' ? (
-                <button name={name} className={styles.ButtonAlt} value={text} onClick={action}>
-					{action}
-                    {text}
-                </button>
-            ) : (
-                <></>
-            )}
-        </>
     );
 };
 
