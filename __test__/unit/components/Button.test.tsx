16b2a44d04cd1d56ed2517155b23ee225f017d64
Unai Bolivar <ujbolivar@gmail.com>
2023-01-06 16:10:09 +0100

    test - Unit tests: Recovered lastest changes to button test file.

--- __test__/unit/components/Button.test.tsx
+++ __test__/unit/components/Button.test.tsx
@@ -2,17 +2,48 @@ import { fireEvent, render, screen } from '@testing-library/react';
 import Button from 'components/Button/Button';
 
 describe('Render Button component', () => {
-    test('Render Button with disabled prop', () => {
+    test("Render 'Añadir' Button and press it", async () => {
         const btnOnClick = jest.fn();
-        render(<Button text="testButton" action={btnOnClick} name={"collar"} category="electronics" variant="button" />);
-        expect(screen.getByText(/testButton/i)).toBeInTheDocument();
-    });
-
-    test('Render Button and press it', async () => {
-        const btnOnClick = jest.fn();
-        render(<Button text="testButton" action={btnOnClick} name={"collar"} category="electronics" variant="button" />);
+        render(
+            <Button
+                text="testButton"
+                action={btnOnClick}
+                name={'collar'}
+                category="jewelery"
+                variant="button"
+            />,
+        );
         const button = await screen.getByText(/testButton/i);
         const clicked = await fireEvent.click(button);
         expect(clicked).toBe(true);
+    });
+
+    test("Render 'más colores' Button and press it", async () => {
+        const btnOnClick = jest.fn();
+        render(
+            <Button
+                text="testButton"
+                action={btnOnClick}
+                name={'más colores'}
+                category="jewelry"
+                variant="buttonAlt"
+            />,
+        );
+        const button = await screen.getByText(/testButton/i);
+        const clicked = await fireEvent.click(button);
+        expect(clicked).toBe(true);
+    });
+
+    test("Hide 'más colores' button's visibility if category = 'electronics'", async () => {
+        render(
+            <Button
+                text="testButton"
+                action={onclick}
+                name={'más colores'}
+                category="electronics"
+                variant="buttonAlt"
+            />,
+        );
+        expect(screen.getByText(/testButton/i)).not.toBeVisible();
     });
 });
