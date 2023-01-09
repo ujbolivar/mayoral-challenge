a4e587699aee3de75b1af04ad6e63e9ed053b4a8
Unai Bolivar <ujbolivar@gmail.com>
2023-01-09 15:17:26 +0100

    test: fixed button tests

--- __test__/unit/components/Button.test.tsx
+++ __test__/unit/components/Button.test.tsx
@@ -3,76 +3,52 @@ import Button from 'components/Button/Button';
 
 describe('Render Button component', () => {
     test("Render 'Añadir' button and check if it is in the document", async () => {
-        render(
-            <Button
-                text="testButton"
-                action={onclick}
-                name={'collar'}
-                category="jewelery"
-                variant="button"
-            />,
-        );
-        const button = await screen.getByText(/testButton/i);
+        render(<Button action={onclick} name={'AÑADIR'} category="jewelery" variant="button" />);
+        const button = await screen.getByText(/AÑADIR/i);
         expect(button).toBeInTheDocument();
     });
 
-	test("Render 'Añadir' button and press it", async () => {
+    test("Render 'Añadir' button and press it", async () => {
         const btnOnClick = jest.fn();
-        render(
-            <Button
-                text="testButton"
-                action={btnOnClick}
-                name={'collar'}
-                category="jewelery"
-                variant="button"
-            />,
-        );
-        const button = await screen.getByText(/testButton/i);
+        render(<Button action={btnOnClick} name={'AÑADIR'} category="jewelery" variant="button" />);
+        const button = await screen.getByText(/AÑADIR/i);
         const clicked = await fireEvent.click(button);
         expect(clicked).toBe(true);
     });
 
     test("Render 'más colores' button and check if it is in the document", async () => {
         render(
-            <Button
-                text="testButton"
-                action={onclick}
-                name={'más colores'}
-                category="jewelry"
-                variant="buttonAlt"
-            />,
+            <Button action={onclick} name={'más colores'} category="jewelry" variant="buttonAlt" />,
         );
-        const button = await screen.getByText(/testButton/i);
+        const button = await screen.getByText(/más colores/i);
         expect(button).toBeInTheDocument();
     });
 
-	test("Render 'más colores' button and press it", async () => {
+    test("Render 'más colores' button and press it", async () => {
         const btnOnClick = jest.fn();
         render(
             <Button
-                text="testButton"
                 action={btnOnClick}
                 name={'más colores'}
                 category="jewelry"
                 variant="buttonAlt"
             />,
         );
-        const button = await screen.getByText(/testButton/i);
+        const button = await screen.getByText(/más colores/i);
         const clicked = await fireEvent.click(button);
         expect(clicked).toBe(true);
     });
 
-    test("Check if className is set to .ButtonNone which hides the button from the screen", async () => {
+    test('Check if className is set to .ButtonNone which hides the button from the screen', async () => {
         render(
             <Button
-                text="testButton"
                 action={onclick}
                 name={'más colores'}
                 category="electronics"
                 variant="buttonAlt"
             />,
         );
-        const button = await screen.getByText(/testButton/i);
+        const button = await screen.getByText(/más colores/i);
         expect(button).toHaveClass('ButtonNone');
     });
 });
