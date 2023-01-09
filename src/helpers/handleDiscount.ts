cad0d35bc4acb869c9e839b55a2b733178e8827d
Unai Bolivar <ujbolivar@gmail.com>
2023-01-09 18:14:45 +0100

    finished

--- src/helpers/handleDiscount.ts
+++ src/helpers/handleDiscount.ts
@@ -1,8 +1,12 @@
-// import { Product } from '@/types/all_typedefs';
+// Checks product and assigns a discount flag
 
 const isActive = (id) => (id % 2 === 0 ? true : false);
 
+// Applies discount to price
+
 const applyDiscount = (price, value) => (price - price * (value / 100)).toFixed(2);
+
+// Handles discount
 
 export const handleDiscount = (id, price, discount, setFlag, setTotal) => {
     if (isActive(id)) {
